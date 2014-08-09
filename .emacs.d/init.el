03d941f3d21705baf49342dcc6aa2ef7318b4a6e
hkurokawa <hiroshi.kurokawa@gunosy.com>
2014-08-09 23:59:21 +0900

    Add a utility method to toggle transparency.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -438,15 +438,19 @@
 ;;; Quickrun
 (when (require 'quickrun nil t)
   (global-set-key "\C-cr" 'quickrun))
+
+;;; Util
+(eval-when-compile (require 'cl))
+(defun toggle-transparency ()
+  (interactive)
+  (if (/=
+       (cadr (frame-parameter nil 'alpha))
+       100)
+      (set-frame-parameter nil 'alpha '(100 100))
+    (set-frame-parameter nil 'alpha '(85 50))))
+(global-set-key (kbd "C-c t") 'toggle-transparency)
+
+;;; General format
+;; Disable tab format
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(indent-tabs-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
