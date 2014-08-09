787118df0e948404d03071bc86ea291833508228
hkurokawa <hiroshi.kurokawa@gunosy.com>
2014-08-09 15:48:35 +0900

    Disable tab mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -436,3 +436,15 @@
 ;;; Quickrun
 (when (require 'quickrun nil t)
   (global-set-key "\C-cr" 'quickrun))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(indent-tabs-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
