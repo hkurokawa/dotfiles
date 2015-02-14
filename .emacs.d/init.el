054c6c0070d37ffd02db42d1fc9f6bcb9a7af34f
hkurokawa <hiroshi.kurokawa@gunosy.com>
2015-02-14 19:23:16 +0900

    Add utility methods for HTML.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -478,6 +478,27 @@
   "Sets the transparency of the frame window. 0=transparent/100=opaque"
   (interactive "nTransparency Value 0 - 100 opaque:")
   (set-frame-parameter (selected-frame) 'alpha value))
+;; Clean up
+(defun untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+(defun indent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer.
+Including indent-buffer, which should not be called automatically on save."
+  (interactive)
+  (untabify-buffer)
+  (delete-trailing-whitespace)
+  (indent-buffer))
+(global-set-key (kbd "C-c n") 'cleanup-buffer)
+;;; Zencoding
+;; Auto-start on any markup modes
+(when (require 'zencoding-mode nil t)
+  (add-hook 'sgml-mode-hook 'zencoding-mode)
+  (define-key zencoding-mode-keymap (kbd "C-c C-j") 'zencoding-expand-line)
+  (define-key zencoding-mode-keymap (kbd "C-j") nil))
 
 ;;; Setting for anything
 (defvar org-directory "")
